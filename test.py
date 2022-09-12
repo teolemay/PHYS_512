ca586add13b9943062c817dc69a65084dc95bdc8
teolemay <teolemay@gmail.com>
2022-09-12 13:58:34 -0400

    test python file

---
+++ test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+text = 'hello world'
+
+x = np.arange(0, 100)
+y = np.sin(x/10)
+
+plt.figure()
+plt.plot(x, y, 'r+')
+plt.text(40, 0, text.capitalize())
+plt.show()
