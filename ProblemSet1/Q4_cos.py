ea9cbb5e6c8a89c67ef2a0e0bc4c801bb0b8d7d3
teolemay <teolemay@gmail.com>
2022-09-16 04:18:38 -0400

    Q4 done

---
+++ ProblemSet1/Q4_cos.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate
+
+
+fun = np.cos
+# evaluate function over 11 points to use for the interpolations
+xsample = np.linspace(-np.pi/2, np.pi/2, 5)
+ysample = fun(xsample)
+#x array to plot the interpolations, truth
+xeval = np.linspace(xsample[0]+1e-15, xsample[-1], 500)
+ytruth = fun(xeval)
+
+#polynomial interpolation (cubic polynomial over each interval)
+# this part uses code copied and adapted from the lecture slides for the cubic polynomial (interpolate_integrate.pdf)
+poly = np.polyfit(xsample, ysample, 4)
+ypoly = np.polyval(poly, xeval) #evaluates the polynomial using coefficients poly
+
+plt.figure()
+plt.title('Fourth order polynomial interpolation')
+plt.plot(xeval, ytruth, label = r'$\cos{x}$')
+plt.plot(xeval, ypoly, '--', label='interpolation')
+txt = 'Mean error: ' + "{:e}".format(np.mean(np.abs(ytruth-ypoly)))
+plt.text(0, 0, txt)
+plt.ylabel(r'$y$')
+plt.xlabel(r'$y$')
+plt.legend()
+
+#cubic spline interpolation
+spline = interpolate.CubicSpline(xsample, ysample)
+yspline = spline(xeval)
+
+plt.figure()
+plt.title('Cubic spline interpolation')
+plt.plot(xeval, ytruth, label = r'$\cos{x}$')
+plt.plot(xeval, yspline, '--', label='interpolation')
+txt = 'Mean error: ' + "{:e}".format(np.mean(np.abs(ytruth-yspline)))
+plt.text(0, 0, txt)
+plt.ylabel(r'$y$')
+plt.xlabel(r'$y$')
+plt.legend()
+
+#rational function interpolation
+# this part uses code directly copied from the lecture slides (interpolate_integrate.pdf)
+def rat_eval(p, q, x):
+    top=0
+    for i in range(len(p)):
+        top = top+p[i]*x**i
+    bot=1
+    for i in range(len(q)):
+        bot = bot+q[i]*x**(i+1)
+    return top/bot
+
+def rat_fit(x, y, n, m):
+    assert(len(x) == n+m-1)
+    assert(len(y) == len(x))
+    mat = np.zeros((n+m-1, n+m-1))
+    for i in range(n):
+        mat[:, i] = x**i
+    for i in range(1, m):
+        mat[:, i-1+n]=-y*x**i
+    pars = np.dot(np.linalg.inv(mat), y)
+    p = pars[:n]
+    q = pars[n:]
+    return p, q
+
+n = 3
+m = 3
+p, q = rat_fit(xsample, ysample, n, m)
+yrat = rat_eval(p, q, xeval)
+
+plt.figure()
+plt.title('Rational function interpolation')
+plt.plot(xeval, ytruth, label = r'$\cos{x}$')
+plt.plot(xeval, yrat, '--', label='interpolation')
+txt = 'Mean error: ' + "{:e}".format(np.mean(np.abs(ytruth-yrat)))
+plt.text(0, 0, txt)
+plt.ylabel(r'$y$')
+plt.xlabel(r'$y$')
+plt.legend()
+plt.show()
