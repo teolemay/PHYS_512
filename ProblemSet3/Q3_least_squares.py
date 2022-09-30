09404c11723b24f19b42537b1ddbe9aa8e4c4914
teolemay <teolemay@gmail.com>
2022-09-30 17:30:20 -0400

    q3 done

--- ProblemSet3/Q3_least_squares.py
+++ ProblemSet3/Q3_least_squares.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 x, y, z = np.loadtxt('dish_zenith.txt', unpack=True)
 
-A = np.zeros((len(x), 4))
+A = np.zeros((len(x), 4), )
 #fill in A
 A[:, 0] = 1
 A[:, 1] = x
@@ -15,6 +15,7 @@ lhs = A.T@A
 rhs = A.T@z
 mfit = np.linalg.inv(lhs)@rhs
 
+#extract model parameters from best fit parameters
 a = mfit[3]
 x_0 = mfit[1]/(-2*a)
 y_0 = mfit[2]/(-2*a)
@@ -24,6 +25,36 @@ print('x_0', x_0)
 print('y_0', y_0)
 print('z_0', z_0)
 
-fig = plt.figure()
-ax = plt.axes(fig, projection='3d')
-ax.plot()s
+#plot surface using parameters
+xplot = np.linspace(np.min(x), np.max(x), 300)
+yplot = np.linspace(np.min(y), np.max(y), 300)
+xplot, yplot = np.meshgrid(xplot, yplot)
+zplot = a*( (xplot-x_0)**2 + (yplot-y_0)**2 ) + z_0
+
+fig = plt.figure( )
+ax = fig.add_subplot(projection='3d')
+dots = ax.scatter(x, y, z, color='r', marker='.', label='data')
+surface = ax.plot_surface(xplot, yplot, zplot, color='tab:blue', alpha=0.4, label='Linear least squares fit')
+surface._facecolors2d = surface._facecolor3d
+surface._edgecolors2d = surface._edgecolor3d
+ax.legend()
+ax.set(xlabel='x (mm)', ylabel='y (mm)', zlabel='z (mm)', title='Target positions and best fit surface')
+
+#use a poor approximation for noise = data - model (assuming model is much better than data as if data very noisy)
+noise = (z - (a*( (x-x_0)**2 + (y-y_0)**2 ) + z_0))
+
+N = np.outer(noise, noise)
+
+sigma= np.diag( np.linalg.inv( A.T@np.linalg.inv(N)@A ))
+print('a error', np.sqrt(sigma[3]))
+
+
+
+
+##residual plot, not needed.
+# fig = plt.figure( )
+# ax = fig.add_subplot(projection='3d')
+# surface = ax.scatter(x, y, noise, color='black', marker='.')
+# ax.set(xlabel='x (mm)', ylabel='y (mm)', zlabel='data - model (mm)', title='Dish data fit residual plot')
+# plt.show()
+
