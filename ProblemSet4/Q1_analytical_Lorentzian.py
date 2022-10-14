bbed13e68bc9ea4958917158e1bd26a53aa7ddd6
teolemay <teolemay@gmail.com>
2022-10-14 15:26:19 -0400

    q1b

--- ProblemSet4/Q1_analytical_Lorentzian.py
+++ ProblemSet4/Q1_analytical_Lorentzian.py
@@ -45,12 +45,22 @@ print(f'Threshold passed after {n_iter} steps')
 print(f'final parameters')
 print(p0)
 
+noise = np.sqrt(np.mean( (d - pred)**2 ))
+# N is I * noise so N^-1 is I*(1/noise).
+# parameter covariance matrix is dm^2 = (A'^T N^-1 A')^-1
+# A' is last grad from the calculation!
+rhs = (grad.T * (1/noise) ) @ grad
+covM = np.linalg.inv(rhs)
+errM = np.sqrt(np.diag(covM))
+print('a error', errM[0])
+print('t0 error', errM[1])
+print('w error', errM[2])
+
 
 plt.figure()
 plt.title(r"Data with Gaussian filter ($\sigma = 20$)")
 plt.plot(t, d, label='Data')
 plt.plot(t, pred, label='Model')
-plt.plot(t, dsmooth, label='Filtered data')
 plt.xlabel('t')
 plt.xticks([0, 0.0001, 0.0002, 0.0003, 0.0004])
 plt.ylabel('d')
